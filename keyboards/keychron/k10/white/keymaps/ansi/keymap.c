64e72847024d38f52a8e5939fd2308bf3ed51795
kkeyfun <KKeyfun@users.noreply.github.com>
2024-04-12 18:24:38 -0700

    fix keymap keycodes

--- keyboards/keychron/k10/white/keymaps/ansi/keymap.c
+++ keyboards/keychron/k10/white/keymaps/ansi/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
     [WIN_BASE] = LAYOUT_ansi(
-      KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,      KC_PSCR,  KC_CRTN,  BL_MOD,
+      KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,      KC_PSCR,  KC_CRTN,  BL_STEP,
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,     KC_INS,   KC_HOME,  KC_PGUP,     KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_BSLS,     KC_DEL,   KC_END,   KC_PGDN,     KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
       KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,                                       KC_P4,    KC_P5,    KC_P6,
@@ -66,12 +66,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [WIN_FN] = LAYOUT_ansi(
-      QK_BOOT,          KC_BRID, KC_BRIU, KC_TASK, KC_FLXP, BL_VAD,  BL_VAI,  KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,   KC_VOLU,     _______,  _______,  BL_TOG,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______,     _______,  _______,  _______,     _______,  _______,  _______,  _______,
-      BL_TOG,  BL_MOD,  BL_VAI,  BL_HUI,  BL_SAI,  BL_SPI,  _______, _______, _______, _______, _______, _______, _______,   _______,     _______,  _______,  _______,     _______,  _______,  _______,  _______,
-      _______, BL_RMOD, BL_VAD,  BL_HUD,  BL_SAD,  BL_SPD,  _______, _______, _______, _______, _______, _______,            _______,                                      _______,  _______,  _______,
-      _______, _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,          _______,                          _______,               _______,  _______,  _______,  _______,
-      _______, _______, _______,                   _______,                                     _______, _______, _______,   _______,     _______,  _______,  _______,     _______,            _______         
+      QK_BOOT,          KC_BRID, KC_BRIU, KC_TASK, KC_FLXP,  BL_DOWN, BL_UP,   KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,  KC_VOLU,     _______,  _______,  BL_TOGG,
+      _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,  _______,     _______,  _______,  _______,     _______,  _______,  _______,  _______,
+      _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,  _______,     _______,  _______,  _______,     _______,  _______,  _______,  _______,
+      _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______,           _______,                                      _______,  _______,  _______,
+      _______, _______, _______, _______, _______, _______,  NK_TOGG, _______, _______, _______, _______,          _______,                         _______,               _______,  _______,  _______,  _______,
+      _______, _______, _______,                   _______,                                      _______, _______, _______,  _______,     _______,  _______,  _______,     _______,            _______         
     ),
 
     /*  Mac layout
@@ -91,21 +91,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
 
   [MAC_BASE] = LAYOUT_ansi( // Mac base layout
-      KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,        KC_MSNP,  KC_SIRI,  BL_MOD,
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,       KC_INS,   KC_HOME,  KC_PGUP,     KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_BSLS,       KC_DEL,   KC_END,   KC_PGDN,     KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
-      KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,                                         KC_P4,    KC_P5,    KC_P6,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,                            KC_UP,                 KC_P1,    KC_P2,    KC_P3,    KC_PENT,
-      KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                                      KC_RGUI, KC_RALT, MO(MAC_FN),KC_RCTL,       KC_LEFT,  KC_DOWN,  KC_RGHT,     KC_P0,              KC_PDOT         
+      KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,       KC_MSNP,  KC_SIRI,  BL_STEP,
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,      KC_INS,   KC_HOME,  KC_PGUP,     KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_BSLS,      KC_DEL,   KC_END,   KC_PGDN,     KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
+      KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,                                        KC_P4,    KC_P5,    KC_P6,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,                           KC_UP,                 KC_P1,    KC_P2,    KC_P3,    KC_PENT,
+      KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                                      KC_RGUI, KC_RALT, MO(MAC_FN),KC_RCTL,      KC_LEFT,  KC_DOWN,  KC_RGHT,     KC_P0,              KC_PDOT         
   ),
 
   [MAC_FN] = LAYOUT_ansi( // Mac Fn overlay	  
-	    QK_BOOT,          KC_BRID, KC_BRIU, KC_TASK, KC_FLXP, BL_VAD,  BL_VAI,  KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,   KC_VOLU,       _______,  _______,  BL_TOG,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______,       _______,  _______,  _______,     _______,  _______,  _______,  _______,
-      BL_TOG,  BL_MOD,  BL_VAI,  BL_HUI,  BL_SAI,  BL_SPI,  _______, _______, _______, _______, _______, _______, _______,   _______,       _______,  _______,  _______,     _______,  _______,  _______,  _______,
-      _______, BL_RMOD, BL_VAD,  BL_HUD,  BL_SAD,  BL_SPD,  _______, _______, _______, _______, _______, _______,            _______,                                        _______,  _______,  _______,
-      _______, _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,          _______,                            _______,               _______,  _______,  _______,  _______,
-      _______, _______, _______,                   _______,                                     _______, _______, _______,   _______,       _______,  _______,  _______,     _______,            _______         
+	    QK_BOOT,          KC_BRID, KC_BRIU, KC_TASK, KC_FLXP, BL_VAD,  BL_VAI,  KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,   KC_VOLU,      _______,  _______,  BL_TOGG,
+      _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,   _______,     _______,  _______,  _______,     _______,  _______,  _______,  _______,
+      _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,   _______,     _______,  _______,  _______,     _______,  _______,  _______,  _______,
+      _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______,            _______,                                      _______,  _______,  _______,
+      _______, _______, _______, _______, _______, _______,  NK_TOGG, _______, _______, _______, _______,          _______,                          _______,               _______,  _______,  _______,  _______,
+      _______, _______, _______,                   _______,                                      _______, _______, _______,   _______,     _______,  _______,  _______,     _______,            _______         
     )
 };
 
